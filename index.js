8972cd8fdb794610dbebce44e7e5aaae4f3f1176
vijayslogin <vjaysonline@gmail.com>
2023-05-05 02:43:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,55 +2,36 @@ var spelling = "";
 var typedSpelling = "";
 
 function startGame() {
-    if($("#result").text().length !== 0) {
+
+    if($("#result").text() !== "Result") {
         $("#result").text("");
         location.reload();
     }
 
-    var imageFolder = './images/'; // replace with the actual file path to your images folder
-    var imageArray = []; // initialize an empty array to store the image filenames
-    var spellingArray = [];
+    var imageArray = ["apple", "ball", "cat", "dog", "elephant", "fish", "grapes", "hen", "ice creem", "juice",
+                      "kite", "lion", "mango", "onion", "parrot", "quilt", "rose", "tiger", "umbrella", 
+                      "van", "watch", "xmas tree", "yoga", "zeebra", "one", "two", "three", "four", "five", "six", "seven",
+                      "eight", "nine", "ten", "elevel", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                      "eighteen", "nineteen", "twenty"]; // initialize an empty array to store the image filenames
 
-    var randomImage = "./images/sixteen.jpg";//
-    spelling = sliceImageName(randomImage).toUpperCase();
-    $("#actualImg").attr("src", randomImage); 
-    console.log(">>>" + typedSpelling + ">>>" + spelling) ;
-  /*
-    // use jQuery's $.get() function to retrieve the list of files in the folder
-    $.get(imageFolder, function(data) {
-        console.log("data >>> " + data);
-      $(data).find('a').each(function() { // loop through each file
-        var filename = $(this).attr('href'); // get the filename
-        if (filename.match(/\.(jpeg|jpg|png|gif)$/)) { // check if the file is an image
-          imageArray.push(filename); // add the filename to the array
-        }
-      });
-  
-      var randomIndex = Math.floor(Math.random() * imageArray.length); // generate a random index
-      var randomImage = "/images/sixteen.jpg";// imageArray[randomIndex]; // get the random image filename
-      console.log("image url >>>> " + randomImage);
-      $("#actualImg").attr("src", randomImage); // add the image to the page
-
-      spelling = sliceImageName(randomImage).toUpperCase();
-      console.log(spelling); // "my-image.jpg"
-    });
-    */
+    var randomIndex = Math.floor(Math.random() * imageArray.length); 
+    spelling = imageArray[randomIndex]; // get the random image filename
+    var imageURL = "./images/" + spelling + ".jpg";
+    $("#actualImg").attr("src", imageURL); // add the image to the page
 }
 
 $("#inputText").keypress(function(event) {
-    // console.log(">>>" + typedSpelling + ">>>" + spelling) ;
     
     if(spelling.length === 0) {
         alert("Start game.!");
         location.reload();
     }else if(typedSpelling.length === 0) {
-        typedSpelling = event.key.toUpperCase();
+        typedSpelling = event.key.toLowerCase();
     }else {
-        typedSpelling = typedSpelling + event.key.toUpperCase();
+        typedSpelling = typedSpelling + event.key.toLowerCase();
     }
 
     if(typedSpelling.length === spelling.length) {
-        // console.log(">>>" + typedSpelling + ">>>" + spelling) ;
         if(spelling.length !== 0 && typedSpelling === spelling) {
             $("#result").text("Correct.!");
             $("#result").css("color", "green");
@@ -60,17 +41,10 @@ $("#inputText").keypress(function(event) {
             $("#result").css("color", "red");
             $("body").css("background-image", "url('images/failure.gif')");
         }
-        // Execute a function after 3 seconds
         setTimeout(function() {
             location.reload();startGame();
         }, 5000);
     }
   });
 
-function sliceImageName (imageURL) {
-
-    var imageUrl = imageURL;
-    return imageUrl.split('/').pop().split(".")[0];
-    console.log(imageName); // "my-image.jpg"
-}
   
