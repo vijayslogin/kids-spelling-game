a78c4d342ed21fbb48898b7b756937794493c65a
vijayslogin <vjaysonline@gmail.com>
2023-05-05 00:53:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ var spelling = "";
 var typedSpelling = "";
 
 function startGame() {
-debugger;
-
-    if($("#result").text() !== "Result here") {
+    if($("#result").text().length !== 0) {
         $("#result").text("");
         location.reload();
     }
-debugger;
-    var imageFolder = "images";// 'images/'; // replace with the actual file path to your images folder
+
+    var imageFolder = './images/'; // replace with the actual file path to your images folder
     var imageArray = []; // initialize an empty array to store the image filenames
     var spellingArray = [];
-debugger;    
+  
     // use jQuery's $.get() function to retrieve the list of files in the folder
     $.get(imageFolder, function(data) {
+        console.log("data >>> " + data);
       $(data).find('a').each(function() { // loop through each file
         var filename = $(this).attr('href'); // get the filename
         if (filename.match(/\.(jpeg|jpg|png|gif)$/)) { // check if the file is an image
           imageArray.push(filename); // add the filename to the array
         }
       });
- debugger;     
+  
       var randomIndex = Math.floor(Math.random() * imageArray.length); // generate a random index
       var randomImage = imageArray[randomIndex]; // get the random image filename
+      console.log("image url >>>> " + randomImage);
       $("#actualImg").attr("src", randomImage); // add the image to the page
-debugger;
+
       spelling = sliceImageName(randomImage).toUpperCase();
       console.log(spelling); // "my-image.jpg"
     });
 }
 
 $("#inputText").keypress(function(event) {
-    console.log(">>>" + typedSpelling + ">>>" + spelling) ;
+    // console.log(">>>" + typedSpelling + ">>>" + spelling) ;
     
     if(spelling.length === 0) {
         alert("Start game.!");
@@ -44,7 +44,7 @@ $("#inputText").keypress(function(event) {
     }
 
     if(typedSpelling.length === spelling.length) {
-        console.log(">>>" + typedSpelling + ">>>" + spelling) ;
+        // console.log(">>>" + typedSpelling + ">>>" + spelling) ;
         if(spelling.length !== 0 && typedSpelling === spelling) {
             $("#result").text("Correct.!");
             $("#result").css("color", "green");
@@ -56,16 +56,15 @@ $("#inputText").keypress(function(event) {
         }
         // Execute a function after 3 seconds
         setTimeout(function() {
-            location.reload();
-            startGame();
+            location.reload();startGame();
         }, 5000);
     }
   });
 
 function sliceImageName (imageURL) {
-    debugger;
+
     var imageUrl = imageURL;
     return imageUrl.split('/').pop().split(".")[0];
-    console.log(imageName); // "my-image.jpg"
+    // console.log(imageName); // "my-image.jpg"
 }
   
